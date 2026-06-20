0c9132c86ba22cdde605c53711b64283274317aa
Sergei Radeckii <radzesy@gmail.com>
2026-06-20 14:40:19 +0300

    Add project sources and workflow

--- src/ComparisonString.java
+++ src/ComparisonString.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class ComparisonString {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
