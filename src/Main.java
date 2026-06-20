59ebe5fd532967d316342a9799048e6949b2007e
Sergei Radeckii <radzesy@gmail.com>
2026-06-20 18:37:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        for (int j : arr) {
+            if (j % 2 == 0) {
+                System.out.println(j);
+            }
+        }
+    }
+}
